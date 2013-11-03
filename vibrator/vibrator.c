a5d0f69962f1ffb610acd1ed56b0c2880e018e5f
KINGbabasula <kingbabasula@gmail.com>
2013-11-03 11:13:49 +0100

    Update vibrator.c

--- vibrator/vibrator.c
+++ vibrator/vibrator.c
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <hardware_legacy/vibrator.h>
 #include "qemu.h"
 
 #include <stdio.h>
@@ -21,22 +20,11 @@
 #include <fcntl.h>
 #include <errno.h>
 
-
-#ifdef USE_ALTERNATIVE_VIBRATOR
-extern int sendit(int timeout_ms);
-#else
-
 #define THE_DEVICE "/sys/class/timed_output/vibrator/enable"
 
 int vibrator_exists()
 {
     int fd;
-
-#ifdef QEMU_HARDWARE
-    if (qemu_check()) {
-        return 1;
-    }
-#endif
 
     fd = open(THE_DEVICE, O_RDWR);
     if(fd < 0)
@@ -50,12 +38,6 @@ static int sendit(int timeout_ms)
     int nwr, ret, fd;
     char value[20];
 
-#ifdef QEMU_HARDWARE
-    if (qemu_check()) {
-        return qemu_control_command( "vibrator:%d", timeout_ms );
-    }
-#endif
-
     fd = open(THE_DEVICE, O_RDWR);
     if(fd < 0)
         return errno;
@@ -68,15 +50,17 @@ static int sendit(int timeout_ms)
     return (ret == nwr) ? 0 : -1;
 }
 
-#endif
 
 int vibrator_on(int timeout_ms)
 {
     /* constant on, up to maximum allowed time */
-    return sendit(timeout_ms);
+        if(timeout_ms < 0)
+                return sendit(15000);
+        return sendit(timeout_ms);
 }
 
 int vibrator_off()
-{
-    return sendit(0);
+{        
+        /* checks if the device is vibrating, if it's vibrating stop, if not, do nothing */
+        return (vibrator_exists() == 0) ? 0 : 1;
 }
