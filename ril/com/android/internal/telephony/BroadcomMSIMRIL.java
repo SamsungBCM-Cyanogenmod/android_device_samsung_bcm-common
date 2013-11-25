53f1dbca8006f9b5bfb17d7ca0c8ea94439bf350
KINGbabasula <kingbabasula@gmail.com>
2013-11-25 16:39:32 +0100

    Update BroadcomMSIMRIL.java

--- ril/com/android/internal/telephony/BroadcomMSIMRIL.java
+++ ril/com/android/internal/telephony/BroadcomMSIMRIL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2013 The CyanogenMod Project
+ * Copyright (C) 2012 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,94 +16,76 @@
 
 package com.android.internal.telephony;
 
-import android.content.Context;
-import android.os.Message;
+import static com.android.internal.telephony.RILConstants.*;
 
-/**
- * Backwards compatible RIL implementation for Broadcom MSIM based
- * radios. Android 4.3 added the CELL_INFO_LIST commands, displacing several
- * command ids already used in pre-4.3 RILs.
- * 
+import android.content.Context;
+import android.os.AsyncResult;
+import android.os.Message;
+import android.os.Parcel;
+import android.text.TextUtils;
+import android.telephony.Rlog;
+
+import com.android.internal.telephony.dataconnection.DataCallResponse;
+
+/*
+ * Custom Broadcom No SimReady RIL for Samsung BCM28155 Radio
+ *
  * {@hide}
  */
-public class BroadcomMSIMRIL extends RIL implements CommandsInterface {
-
-    static final int RIL_REQUEST_IMS_REGISTRATION_STATE = 109;
-    static final int RIL_REQUEST_IMS_SEND_SMS = 110;
-    static final int RIL_REQUEST_GET_DATA_CALL_PROFILE = 111;
-    static final int RIL_REQUEST_SET_UICC_SUBSCRIPTION = 118;
-    static final int RIL_REQUEST_SET_DATA_SUBSCRIPTION = 119;
-    static final int RIL_REQUEST_GET_UICC_SUBSCRIPTION = 120;
-    static final int RIL_REQUEST_GET_DATA_SUBSCRIPTION = 121;
-    static final int RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED = 1036;
-    static final int RIL_UNSOL_TETHERED_MODE_STATE_CHANGED = 1037;
-
-    public BroadcomMSIMRIL(Context context, int networkMode,
-            int cdmaSubscription) {
+public class SamsungBCMRIL extends RIL implements CommandsInterface {
+    public SamsungBCMRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
+        mQANElements = 5;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void getCellInfoList(Message result) {
-        if (RILJ_LOGD) riljLog("[STUB] > getCellInfoList");
-    }
+    public void
+    dial(String address, int clirMode, UUSInfo uusInfo, Message result) {
+        RILRequest rr = RILRequest.obtain(RIL_REQUEST_DIAL, result);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setCellInfoListRate(int rateInMillis, Message response) {
-        if (RILJ_LOGD) riljLog("[STUB] > setCellInfoListRate");
-    }
+        rr.mParcel.writeString(address);
+        rr.mParcel.writeInt(clirMode);
 
-    public void getImsRegistrationState(Message result) {
-        RILRequest rr = RILRequest.obtain(RIL_REQUEST_IMS_REGISTRATION_STATE, result);
-
-        if (RILJ_LOGD) {
-            riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+        if (uusInfo == null) {
+            rr.mParcel.writeInt(0); // UUS information is absent: Samsung BCM compat
+        } else {
+            rr.mParcel.writeInt(1); // UUS information is present
+            rr.mParcel.writeInt(uusInfo.getType());
+            rr.mParcel.writeInt(uusInfo.getDcs());
+            rr.mParcel.writeByteArray(uusInfo.getUserData());
         }
-        send(rr);
-    }
+        rr.mParcel.writeInt(255);
 
-    public void setUiccSubscription(int slotId, int appIndex, int subId,
-            int subStatus, Message result) {
-        //Note: This RIL request is also valid for SIM and RUIM (ICC card)
-        RILRequest rr = RILRequest.obtain(RIL_REQUEST_SET_UICC_SUBSCRIPTION, result);
-
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
-                + " slot: " + slotId + " appIndex: " + appIndex
-                + " subId: " + subId + " subStatus: " + subStatus);
-
-        rr.mParcel.writeInt(slotId);
-        rr.mParcel.writeInt(appIndex);
-        rr.mParcel.writeInt(subId);
-        rr.mParcel.writeInt(subStatus);
-
-        send(rr);
-    }
-
-    public void setDataSubscription(Message result) {
-        RILRequest rr = RILRequest.obtain(RIL_REQUEST_SET_DATA_SUBSCRIPTION, result);
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+
         send(rr);
     }
 
     public void
-    getDataCallProfile(int appType, Message result) {
-        RILRequest rr = RILRequest.obtain(
-                RIL_REQUEST_GET_DATA_CALL_PROFILE, result);
+    setNetworkSelectionMode(String operatorNumeric, Message response) {
+        RILRequest rr;
 
-        // count of ints
-        rr.mParcel.writeInt(1);
-        rr.mParcel.writeInt(appType);
+        if (operatorNumeric == null)
+            rr = RILRequest.obtain(RIL_REQUEST_SET_NETWORK_SELECTION_AUTOMATIC, response);
+        else
+            rr = RILRequest.obtain(RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL, response);
 
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
-                + " : " + appType);
+        rr.mParcel.writeString(operatorNumeric);
+        rr.mParcel.writeInt(-1);
 
         send(rr);
+    }
+
+    @Override
+    public void
+    setNetworkSelectionModeAutomatic(Message response) {
+        setNetworkSelectionMode(null, response);
+    }
+
+    @Override
+    public void
+    setNetworkSelectionModeManual(String operatorNumeric, Message response) {
+        setNetworkSelectionMode(operatorNumeric, response);
     }
 
 }
